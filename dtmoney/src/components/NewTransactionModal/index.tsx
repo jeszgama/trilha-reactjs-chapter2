59ad84fbc73ce59a075a8d675fcaef7057ba653c
“jessica” <jessicagama9827@gmail.com>
2021-10-19 13:08:11 -0400

    --inserindo transações na API

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,7 @@ import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg';
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -15,18 +16,19 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
     const [title, setTitle] = useState('');
     const [value, setValue] = useState(0);
     const [category, setCategory] = useState('');
-
     const [type, setType] = useState('deposit');
     
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        console.log({
+        const data = {
             title,
             value,
             category,
             type,
-        })
+        };
+
+        api.post('/transactions', data)
     }
 
     return(
